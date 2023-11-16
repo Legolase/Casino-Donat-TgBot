bce9aad3a5b63fab640c5d60d9c894bdef8bad2f
Legy <nikita.larsky@gmail.com>
2023-11-16 15:41:12 +0300

    recover GameManager.cpp

--- GameManager.h
+++ GameManager.h
@@ -5,7 +5,6 @@
 #ifndef CASINO_GAMEMANAGER_H
 #define CASINO_GAMEMANAGER_H
 
-#include "Chat.h"
 #include "Game.h"
 #include "GameEventHandler.h"
 #include <thread>
@@ -23,21 +22,10 @@ public:
   // returns message_id
   int32_t sendMessage(int64_t group_id, int64_t bt);
 private:
-  void load_data();
-  void save_data();
-
-  Chat* getChatByRequest(GameRequest const& request);
-  Game* getGame(const GameRequest& request, Chat const& chat);
 
   std::map<int64_t, Game> current_games;
-  std::map<int64_t, Chat> chat_settings;
   std::thread worker;
   TgBot::Bot& bot;
-
-  Chat default_chat;
-  Game default_game;
 };
-
-static_assert(sizeof(GameManager) == 0);
 
 #endif // CASINO_GAMEMANAGER_H
